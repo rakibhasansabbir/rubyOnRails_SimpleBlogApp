f77202446695674c26a46f384f3bc392b85e670e
sabbirsr <rakibhasansabbir@yahoo.com>
2018-08-16 02:19:53 +0600

    store data to SQL lite

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def about
         @title = 'about us';
+        @content = 'if you are interested to know about me please contact our team.
+        Thank you :)';
     end
+
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
     def index
         
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def new
+
+    end
+
+    def create
+        # render plain: params[:post].inspect
+        @post = Post.new(post_param);
+
+        @post.save
+        redirect_to @post
+    end
+
+    private def post_param
+        params.require(:post).permit(:title,:body)
+    end
 end
