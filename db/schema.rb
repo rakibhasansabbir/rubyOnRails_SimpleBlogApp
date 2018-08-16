64a22f51a21e67793d0580cc6f0b32767dd5d461
sabbirsr <rakibhasansabbir@yahoo.com>
2018-08-17 00:57:08 +0600

    add user comment section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815193638) do
+ActiveRecord::Schema.define(version: 20180816171920) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "username"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
