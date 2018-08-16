cd5fa298f7c36b2c8bb08d1a60e470c9f0517bde
sabbirsr <rakibhasansabbir@yahoo.com>
2018-08-16 23:16:09 +0600

    setup edit also delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,28 @@ class PostsController < ApplicationController
         end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+      @post = Post.find(params[:id])
+
+      if(@post.update(post_param))
+        redirect_to @post
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to posts_path
+
+    end
+
     private def post_param
         params.require(:post).permit(:title,:body)
     end
